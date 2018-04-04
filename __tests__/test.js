e5205f7805d3d2a87035a58d26dc6759358c860c
y.a <yankin.aa@mail.ru>
2018-04-04 18:06:02 +0300

    level4

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,9 +1,8 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
-
 test('changing the contents of .json files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
   const pathToFile1 = '__tests__/__fixtures__/json/before.json';
   const pathToFile2 = '__tests__/__fixtures__/json/after.json';
   const decision = genDiff(pathToFile1, pathToFile2);
@@ -12,8 +11,18 @@ test('changing the contents of .json files', () => {
 });
 
 test('changing the contents of .yaml files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
   const pathToFile1 = '__tests__/__fixtures__/yaml/before.yml';
   const pathToFile2 = '__tests__/__fixtures__/yaml/after.yml';
+  const decision = genDiff(pathToFile1, pathToFile2);
+
+  expect(decision).toBe(result);
+});
+
+test('changing the contents of .ini files', () => {
+  const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
+  const pathToFile1 = '__tests__/__fixtures__/ini/before.ini';
+  const pathToFile2 = '__tests__/__fixtures__/ini/after.ini';
   const decision = genDiff(pathToFile1, pathToFile2);
 
   expect(decision).toBe(result);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import _ from 'lodash';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import path from 'path';
 
 const parsers = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default (pathToFile1, pathToFile2) => {
-  const GetpathToFile1 =  fs.readFileSync(pathToFile1);
-  const GetpathToFile2 =  fs.readFileSync(pathToFile2);
+  const getpathToFile1 =  fs.readFileSync(pathToFile1, 'utf-8');
+  const getpathToFile2 =  fs.readFileSync(pathToFile2, 'utf-8');
 
   const getFormat = path.extname(pathToFile1);
 
   const parse = parsers[getFormat];
 
-  const firstObj = parse(GetpathToFile1);
-  const lastObj = parse(GetpathToFile2);
+  const firstObj = parse(getpathToFile1);
+  const lastObj = parse(getpathToFile2);
 
   const getLine = (sing, name, value) => `${sing}${name}: ${value}\n`;
 
-  const arr = Array.from(new Set([...Object.keys(firstObj), ...Object.keys(lastObj)]));
+  const joinArrays = Array.from(new Set([...Object.keys(firstObj), ...Object.keys(lastObj)]));
 
-  const result = arr.map((elem) => {
+  const result = joinArrays.map((elem) => {
     if (_.has(firstObj, elem) && _.has(lastObj, elem) && firstObj[elem] !== lastObj[elem]) {
       return `${getLine('  + ', elem, lastObj[elem])}${getLine('  - ', elem, firstObj[elem])}`;
     } else if (!_.has(firstObj, elem) && _.has(lastObj, elem)) {
