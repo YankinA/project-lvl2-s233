113378e1c98d847f8fdeaec0e20663ef171f7ea6
y.a <yankin.aa@mail.ru>
2018-04-08 21:00:12 +0300

    level7.7

--- __tests__/test.js
+++ __tests__/test.js
@@ -76,8 +76,8 @@ test('changing the contents  of the nested Plain', () => {
 
 test('changing the contents  of the nested JSON .ini files', () => {
   const result = fs.readFileSync('__tests__/__fixtures__/json.json', 'utf8');
-  const pathToFile1 = '__tests__/__fixtures__/ini/before.ini';
-  const pathToFile2 = '__tests__/__fixtures__/ini/after.ini';
+  const pathToFile1 = '__tests__/__fixtures__/yaml/before.yml';
+  const pathToFile2 = '__tests__/__fixtures__/yaml/after.yml';
   const decision = genDiff(pathToFile1, pathToFile2, 'json');
 
   expect(decision).toBe(result);
